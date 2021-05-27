670dfe9dd1bbbba78e3e434a4c7fc8a218bcdeed
Ricardo Baltazar <56805229+RicardoBaltazar@users.noreply.github.com>
2021-05-26 22:43:32 -0300

    Delete app directory

--- app/src/components/logo/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styled.module.scss';
-
-export default function Logo(){
-    return (
-        <div className={styles.logo}>
-            <h2>DecorDesign</h2>
-        </div>
-    )
-}
